e9d7385a996d0b79b8159b8464efdd7d73dc3b7a
Jeet Soni <sonijeet420@gmail.com>
2023-09-03 15:08:10 +0530

    Add custom texonomy named book category

--- includes/custom-post-type.php
+++ includes/custom-post-type.php
@@ -37,3 +37,37 @@ function wpb_register_book_post_type() {
 // Hook the custom post type registration function to the 'init' action
 add_action('init', 'wpb_register_book_post_type');
 
+// Register the custom hierarchical taxonomy "Book Category"
+function register_book_category_taxonomy() {
+    $labels = array(
+        'name' => 'Book Categories',
+        'singular_name' => 'Book Category',
+        'search_items' => 'Search Book Categories',
+        'all_items' => 'All Book Categories',
+        'parent_item' => 'Parent Book Category',
+        'parent_item_colon' => 'Parent Book Category:',
+        'edit_item' => 'Edit Book Category',
+        'update_item' => 'Update Book Category',
+        'add_new_item' => 'Add New Book Category',
+        'new_item_name' => 'New Book Category Name',
+        'menu_name' => 'Book Categories',
+    );
+
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => 'book-category',
+        'rewrite' => array('slug' => 'book-category'),
+    );
+
+    register_taxonomy('book_category', 'book', $args);
+}
+add_action('init', 'register_book_category_taxonomy');
+
+// Associate the "Book Category" taxonomy with the "Book" custom post type
+function associate_taxonomy_with_post_type() {
+    register_taxonomy_for_object_type('book_category', 'book');
+}
+add_action('init', 'associate_taxonomy_with_post_type');
